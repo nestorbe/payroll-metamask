77bb160cf18776f3fd1ac8352ada1ff5ae498277
nestorbe <nestorbarboza@gmail.com>
2020-11-30 17:18:05 -0500

    second commit

--- src/services/web3.js
+++ src/services/web3.js
@@ -1,13 +1,5 @@
 import Web3 from 'web3';
-import detectEthereumProvider from '@metamask/detect-provider';
 
-const provider = detectEthereumProvider();
-const web3 = () => {
-  if(provider) {
-    new Web3(window.web3.currentProvider);
-  } else {
-    alert('Please install Metamask to use this application!')
-  }
-}
+const web3 = new Web3(window.web3.currentProvider);
 
 export default web3;
