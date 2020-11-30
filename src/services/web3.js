84ed9eeaa39f51d599f83cbaeb123a073ae31537
nestorbe <nestorbarboza@gmail.com>
2020-11-30 17:39:48 -0500

    web3 fix

--- src/services/web3.js
+++ src/services/web3.js
@@ -1,5 +1,11 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (window.web3 === undefined) {
+  web3 = new Web3('wss://mainnet.infura.io/ws');
+} else {
+  web3 = new Web3(window.web3.currentProvider);
+}
 
 export default web3;
